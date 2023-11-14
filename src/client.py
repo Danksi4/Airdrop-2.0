172b88d96139792f8072df5cbde6a5213fcd9bf6
Daniel Dean <danielcarterdean@gmail.com>
2023-11-14 16:06:59 -0700

    commit code

--- src/client.py
+++ src/client.py
@@ -1,2 +1,34 @@
+import tqdm
+import os
+import socket
 
+SEPERATOR = '<SEPERATOR>'
+BUFFER_SIZE = 4096
 
+host = '192.168.64.4'
+port = 5001
+filename = 'text.txt'
+# get the size of the file in bytes
+filesize = os.path.getsize(filename)
+# create the client socket
+s = socket.socket()
+# connecting to the server
+print(f'[+] Connecting to {host}:{port}...')
+s.connect((host, port))
+print('Connected')
+# send the file name and file size information to the server
+s.send(f'{filesize}{SEPERATOR}{filename}'.encode()) # calls encode() to convert the information to bytes
+
+## SEND THE FILE AND DISPLAY THE PROGRESS
+progress = tqdm.tqdm(range(filesize), f'Sending {filename}', unit = 'B', unit_scale = True, unit_divisor = 1024) # creates the progress bar for file transmission
+with open(filename, 'rb') as f: # opens the file to read binary 
+    while True:
+        # read the bytes from the file in 4096 byte increments as specified by BUFFER_SIZE
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            break # file is done sending
+        s.sendall(bytes_read) # sendall to assure file is transmitted
+        # update the progress bar based on the amount of bytes read
+        progress.update(len(bytes_read))
+#close the socket
+s.close()
