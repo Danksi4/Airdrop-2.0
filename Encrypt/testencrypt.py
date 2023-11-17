8a420a2d1b5f120981783b83737e849df7dd09c2
a-alv3s <alves1@ualberta.ca>
2023-11-16 22:35:52 -0700

    Fixed folder

--- Encrypt/testencrypt.py
+++ Encrypt/testencrypt.py
@@ -7,11 +7,11 @@ class encryption():
         self.name=name
         self.ID=str(random.random())
         self.key=Fernet.generate_key()
-        with open('tempkeys/key.key'+self.ID,'wb') as key_file:
+        with open('tempkeys/key#'+self.ID,'wb') as key_file:
             key_file.write(self.key)
 
     def load_key(self):
-        load=open('tempkeys/key.key'+self.ID,'rb').read()
+        load=open('tempkeys/key#'+self.ID,'rb').read()
         return load
     
     def encrypt(self):
@@ -28,11 +28,8 @@ class encryption():
     
 
 '''Test example on how to call, need to figure out why it creates the same key when called withh diff objects.'''
-message=input('First message: ')
-message2=input('Second message: ')
+message=input('Message to be encrypted: ')
 keytest=encryption(message)
-keytest2=encryption(message2)
-keytest2.load_key()
 print("The key is:",keytest.load_key())
 x=keytest.encrypt()
 print('The encrypted message is:',x)
