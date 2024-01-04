59bfd9d631b5ebdde81730642c9b542b5c510df6
Bradley Bravender <bsbraven@ualberta.ca>
2024-01-03 18:44:52 -0700

    Commit

--- Test/main_gui.py
+++ Test/main_gui.py
@@ -1,3 +1,17 @@
-import database
+from database import Database
+import customtkinter
 
-x = database.Database()
+class App(customtkinter.CTk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Airdrop 2.0")
+        self.geometry("800x600")
+  
+    def start(self):
+        self.mainloop()
+
+if __name__=="__main__":
+    #x = Database()
+    program = App()
+    program.start()
